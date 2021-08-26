5e6ce036d6f21a950725e2b90d609dfc2b0529f4
Stackofsugar <digno.christopher@gmail.com>
2021-08-26 21:55:55 +0700

    Add project files.

---
+++ src/TugasPraktikum_1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+    string m_student_id;
+protected:
+    string m_name;
+    int m_age;
+public:
+    Mahasiswa(string, int);
+    string getName() { return m_name; };
+    void setName(string);
+};
+
+Mahasiswa::Mahasiswa(string name, int age) {
+    m_name = name;
+    m_age = age;
+}
+
+void Mahasiswa::setName(string name) {
+    m_name = name;
+}
+
+int main() {
+    Mahasiswa miko("Michael Raditya Krisnadhi", 18);
+    cout << miko.getName() << '\n';
+    miko.setName("Miko");
+    cout << miko.getName() << '\n';
+}
